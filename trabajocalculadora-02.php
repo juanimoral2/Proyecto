ecc9e244fab08a33fe73d3d24925ec665aeadd32
unknown <juan20.cabj@gmail.com>
2023-03-21 00:03:32 -0300

    calculadora

---
+++ trabajocalculadora-02.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trabajo Practico</title>
+</head>
+<body>
+
+    <?php
+        echo     '<h1>Demostracion de uso de codigos</h1>';
+        echo '<h2> intento de calculadora </h2>';
+
+//$_GET['numero1'];
+    $numero = 10;
+    $numero2 = 0;
+
+    echo 'La suma de los numeros es: '.$numero+$numero2;
+
+    echo '<br/>'; 
+    echo 'la resta de los numeros es : '.$numero-$numero2;
+    echo'<br/>';
+    echo 'la multiplicacion de los numeros es : '.$numero*$numero2;
+     echo'<br/>';
+     echo 'la divicion de los numeros es:'.$numero/$numero2;
+
+
+
+      
+     echo '<br/>';
+
+
+
+?>
+
+</body>
+</html>
+
+
+
